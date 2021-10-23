924386cb37e51ee173c2a07d4b13527e72536ad0
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-23 15:20:45 +0530

    myFunction() added

--- x_archive/The Dice Game/index.js
+++ x_archive/The Dice Game/index.js
@@ -2,5 +2,33 @@ var number = [1, 2, 3, 4, 5, 6];
 var numberLength = number.length;
 var randomNumberPlayer1 = Math.floor(Math.random() * numberLength);
 var randomNumberPlayer2 = Math.floor(Math.random() * numberLength);
-alert(randomNumberPlayer1);
-alert(randomNumberPlayer2);
+
+function myFunction() {
+    if (randomNumberPlayer1 == 0) {
+        document.getElementById("refresh").innerHTML = "PLAYER 2 WINS!";
+        document.getElementById("player1").innerHTML = "1";
+        document.getElementById("player2").innerHTML = randomNumberPlayer2;
+        document.getElementById("imgDice1").src = "images/dice1.png";
+    }
+    else if (randomNumberPlayer2 == 0) {
+        document.getElementById("refresh").innerHTML = "PLAYER 2 WINS!";
+        document.getElementById("player1").innerHTML = randomNumberPlayer1;
+        document.getElementById("player2").innerHTML = "6";
+        document.getElementById("imgDice2").src = "images/dice6.png";
+    }
+    else if (randomNumberPlayer1 == randomNumberPlayer2) {
+        document.getElementById("refresh").innerHTML = "DRAW";
+        document.getElementById("player1").innerHTML = randomNumberPlayer1;
+        document.getElementById("player2").innerHTML = randomNumberPlayer2;
+    }
+    else if (randomNumberPlayer1 > randomNumberPlayer2) {
+        document.getElementById("refresh").innerHTML = "PLAYER 1 WINS!";
+        document.getElementById("player1").innerHTML = randomNumberPlayer1;
+        document.getElementById("player2").innerHTML = randomNumberPlayer2;
+    }
+    else if (randomNumberPlayer1 < randomNumberPlayer2) {
+        document.getElementById("refresh").innerHTML = "PLAYER 2 WINS!";
+        document.getElementById("player1").innerHTML = randomNumberPlayer1;
+        document.getElementById("player2").innerHTML = randomNumberPlayer2;
+    }
+}
