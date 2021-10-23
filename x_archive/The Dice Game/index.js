95a4befd421427c7c314aea2130be96262654df5
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-23 14:06:16 +0530

    randomNumberPlayer2 added in js

--- x_archive/The Dice Game/index.js
+++ x_archive/The Dice Game/index.js
@@ -1,5 +1,6 @@
 var number = [1, 2, 3, 4, 5, 6];
 var numberLength = number.length;
 var randomNumberPlayer1 = Math.floor(Math.random() * numberLength);
+var randomNumberPlayer2 = Math.floor(Math.random() * numberLength);
 alert(randomNumberPlayer1);
-
+alert(randomNumberPlayer2);
