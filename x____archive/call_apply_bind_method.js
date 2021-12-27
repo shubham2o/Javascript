7d16a45d699618fd4b3f557171625adb20db018c
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-27 23:32:16 +0530

    call_apply_bind_method completed

--- x____archive/call_apply_bind_method.js
+++ x____archive/call_apply_bind_method.js
@@ -1,11 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Call, Apply, Bind method</title>
-</head>
-<body>
-</body>
-</html>
+// call method
+console.log(`||| CALL METHOD |||`);
+let name1 = {
+    firstname : "Akshay",
+    lastname : "Saini",
+}
+
+let printFullName = function (hometown, state) {
+    console.log(this.firstname + " " + this.lastname + " from " + hometown + " , " + state);
+}
+
+printFullName.call(name1, "Dehradun", "Uttarakhand");
+
+let name2 = {
+    firstname : "Sachin",
+    lastname : "Tendulkar",
+}
+
+// Function borrowing
+printFullName.call(name2, "Mumbai", "Maharashtra");
+console.log("");
+
+
+// apply method
+console.log(`||| APPLY METHOD |||`);
+printFullName.apply(name1, ["Dehradun", "Uttarakhand"]);
+
+printFullName.apply(name2, ["Mumbai", "Maharashtra"]);
+console.log("");
+
+
+// bind method
+console.log(`||| BIND METHOD |||`);
+let printMyName = printFullName.bind(name1, "Dehradun", "Uttarakhand");
+console.log(printMyName);
+
+let printMyName1 = printFullName.bind(name2, "Mumbai", "Maharashtra");
+console.log(printMyName1);
+console.log("");
+
+printMyName();
+printMyName1();
