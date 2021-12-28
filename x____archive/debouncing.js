4f58f1d6452ecad200874eaf37cb4ed1bf7fb0f0
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-28 19:01:19 +0530

    debouncing completed

---
+++ x____archive/debouncing.js
@@ -0,0 +1,22 @@
+// Debouncing in JavaScript
+let counter = 0;
+const getData = () => {
+    // calls an API and gets Data
+    console.log("Fetching Data... " + counter++);
+}
+
+const debounce = function (fn, d) {
+    let timer;
+    return function () {
+        let context = this,
+            args = arguments;
+
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            getData.apply(context, arguments);
+        }, d);
+    }
+}
+
+const betterFunction = debounce(getData, 300);
