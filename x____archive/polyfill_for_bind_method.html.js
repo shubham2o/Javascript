d804c637dac22f111b848dfbe0cf35c12106b27c
shubham2o <shubhamsharma.ha@gmail.com>
2021-12-28 13:58:52 +0530

    polyfill for bind method - not completed

---
+++ x____archive/polyfill_for_bind_method.html.js
@@ -0,0 +1,21 @@
+let name1 = {
+    firstname : "Shubham",
+    lastname : "Sharma"
+}
+
+let printName = function () {
+    console.log(this.firstname + " " + this.lastname);
+}
+
+let printMyName = printName.bind(name1);
+printMyName();
+
+Function.prototype.mybind = function (...args) {
+    let obj = this
+    return function () {
+        obj.call(args[0]);
+    }
+}
+
+let printMyName2 = printName.mybind(name1);
+printMyName2(); 
