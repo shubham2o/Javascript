910b2bc7c06f1b30bfe63aea678ef0a62d0a4a3c
shubham2o <shubhamsharma.ha@gmail.com>
2022-01-01 22:42:57 +0530

    block scope and shadowing completed

---
+++ IMPORTANT!/block_scope_&_shadowing.js
@@ -0,0 +1,50 @@
+// let and const are Block Scope and var is a Global Scope
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    // console.log(a);
+    // console.log(b);
+    // console.log(c);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// Shadowing 
+// -------- var --------
+var x = 1000;
+{
+    var x = 10;
+    let y = 20;
+    const z = 30;
+    // console.log(x);
+    // console.log(y);
+    // console.log(z);
+}
+// console.log(x);
+
+// -------- let --------
+let q = 1000;
+{
+    var p = 10;
+    let q = 20;
+    const r = 30;
+    // console.log(p);
+    // console.log(q);
+    // console.log(r);
+}
+// console.log(q);
+
+// -------- const --------
+const w = 1000;
+{
+    var u = 10;
+    let v = 20;
+    const w = 30;
+    console.log(u);
+    console.log(v);
+    console.log(w);
+}
+console.log(w);
