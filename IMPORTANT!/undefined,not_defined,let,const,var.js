3bd7a4f1a1b9e2ab0d2c3c50db391f8053ebd357
shubham2o <shubhamsharma.ha@gmail.com>
2022-01-01 20:11:01 +0530

    undefined, not_defined, let, const, var completed

---
+++ IMPORTANT!/undefined,not_defined,let,const,var.js
@@ -0,0 +1,36 @@
+/* // OUTPUT - not defined
+console.log(x);
+
+let func = function curry() {
+    let c = 30;
+} 
+console.log(c);
+
+let func = function klay() {
+    var k = 11;
+} 
+console.log(k); */
+
+
+/* // OUTPUT - undefined
+console.log(a);
+var a = 100; */
+
+
+/* // OUTPUT - Reference Error
+console.log(b);
+let b = 10; */
+
+
+/* // OUTPUT - missing initializer in const declaration  
+const p;
+p = 1000;
+console.log(p);
+
+let q;
+q = 1000;
+console.log(q);
+
+var r;
+r = 1000;
+console.log(r); */
