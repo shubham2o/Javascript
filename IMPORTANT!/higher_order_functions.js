2e063f5a958a853cf01a7130bb7e8fffca9353d6
shubham2o <shubhamsharma.ha@gmail.com>
2022-01-02 22:32:35 +0530

    higher order functions completed

---
+++ IMPORTANT!/higher_order_functions.js
@@ -0,0 +1,45 @@
+// function y here is Higher Order Function 
+// (A function which take another function as an arguement or returns a function from it)
+function x() {
+    console.log("Namaste");
+}
+function y(x) {
+    x();
+}
+
+
+// Example -->
+const radius = [3, 1, 2, 4];
+
+const area = function (radius) {
+    return Math.PI * radius * radius;
+};
+
+const circumference = function (radius) {
+    return 2 * Math.PI * radius;
+};
+
+const diameter = function (radius) {
+    return 2 * radius;
+};
+
+
+// Array.prototype. will be available on all the array you'll see on our codebase.
+Array.prototype.calculate = function (logic) {
+    const output = [];
+    for (let i = 0; i < this.length; i++) {
+        output.push(logic(this[i]));
+    }
+    return output;
+}
+
+
+console.log(radius.calculate(area));
+console.log(radius.calculate(circumference));
+console.log(radius.calculate(diameter));
+
+console.log(radius.map(area));
+
+// console.log(calculate(radius, area));
+// console.log(calculate(radius, circumference));
+// console.log(calculate(radius, diameter));
