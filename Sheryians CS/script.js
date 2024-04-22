114e9e2a48ca6493ae2a585d49c56e1ae195f362
shubham2o <shubhamsharma.ha@gmail.com>
2024-04-22 13:29:01 +0530

    Till - JS foundation completed

---
+++ Sheryians CS/script.js
@@ -0,0 +1,42 @@
+/* 
+let users = ["Shubham", "Harsh", "Chris"];
+
+console.log(users);
+console.log(users[0]);
+console.log(users.length - 1);
+console.log([users.length - 1]);
+console.log(users[users.length - 1]);
+console.log(users[5]); 
+*/
+
+/*
+let arr = [90909, "Shubham", '8888'];
+
+arr.forEach(function () {
+    console.log("H E L L O");
+})
+
+arr.forEach(function (value, key) {
+    console.log(value, key);
+})
+
+arr.forEach(function (value) {
+    console.log(value, "H E L L O");
+}) 
+*/
+
+/*
+let obj = {};
+let obj1 = new Object();
+
+let obj = {
+    name: "Shubham",
+    age: 23,
+    email: "s@gmail.com",
+    contact: "XXXXXX9090",
+}
+
+console.log(obj);
+console.log(obj.name);
+console.log(obj.contact);
+*/
