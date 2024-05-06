6efb982dc118033419295924d93b7ad9f81786b1
shubham2o <shubhamsharma.ha@gmail.com>
2024-05-06 19:54:34 +0530

    Till - async await added

--- Sheryians CS/3. Asynchronous JS/script.js
+++ Sheryians CS/3. Asynchronous JS/script.js
@@ -37,3 +37,73 @@ console.log("Second");
 console.log("Third");
 */
 
+// Promise()
+/*
+new Promise(function (resolve, reject) {
+    fetch(`https://randomuser.me/api/`)
+        .then(raw => raw.json())
+        .then(data => {
+            if (data.results[0].gender === "male") {
+                resolve();
+            }
+            else {
+                reject();
+            }
+        })
+}).then(function () {
+    console.log("MALE");
+}).catch(function () {
+    console.log("FEMALE");
+})
+
+new Promise(function (resolve, reject) {
+    fetch(`https://randomuser.me/api/`)
+        .then(raw => raw.json())
+        .then(data => {
+            if (data.results[0].gender === "male") resolve();
+            else reject();
+        })
+}).then(function () {
+    console.log("MALE");
+}).catch(function () {
+    console.log("FEMALE");
+})
+
+console.log("First");
+console.log("Second");
+console.log("Third");
+*/
+
+// Callback()
+/*
+function abc(param1, param2) {
+    param2();
+    console.log(param1);
+}
+
+abc("shubham", function () {
+    console.log("This is a callback function");
+})
+
+function getData(url, callback) {
+    fetch(url)
+        .then(raw => raw.json())
+        .then(info => callback(info))
+}
+
+getData(`https://randomuser.me/api/`, function (data) {
+    console.log(data.results[0].email);
+})
+*/
+
+// async - await
+async function abcd() {
+    console.log("First");
+    await fetch(`https://randomuser.me/api/`)
+        .then(raw => raw.json())
+        .then(data => console.log(data.results[0].email));
+    console.log("Second");
+    console.log("Third");
+}
+
+abcd();
