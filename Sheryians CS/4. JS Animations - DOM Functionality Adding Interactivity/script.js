ae093a1ab239e6dbd00805f7005e0162316d678d
shubham2o <shubhamsharma.ha@gmail.com>
2024-05-08 12:51:04 +0530

    JS Animations - DOM Functionality - completed

--- Sheryians CS/4. JS Animations - DOM Functionality Adding Interactivity/script.js
+++ Sheryians CS/4. JS Animations - DOM Functionality Adding Interactivity/script.js
@@ -13,10 +13,42 @@ btn2.addEventListener("click", function () {
     btn2.textContent += ", Shubham";
 })
 
-// innerHTML
+// innerHTML v/s textContent
 let heading1 = document.querySelector("#heading1");
 heading1.innerHTML = "<i>Software Engineer</i>"
 
-// v/s textContent
 let heading2 = document.querySelector("#heading2");
 heading2.textContent = "<i>Working Remotely</i>"
+
+// .style
+let heading3 = document.querySelector("#heading3");
+heading3.style.color = "blue";
+heading3.style.fontFamily = "Monument";
+
+// classList
+let heading4 = document.querySelector('#heading4');
+heading4.classList.add("makeitred");
+// heading4.classList.remove("makeitred");
+
+// document.createElement()
+let heading5 = document.createElement('h1');
+heading5.textContent = "This is made using textContent.";
+heading5.classList.add("makeitred");
+
+// appendChild()
+document.querySelector("body").appendChild(heading5);
+
+// removeChild()
+// document.querySelector("body").removeChild(heading5);
+
+// EventListener()
+let btn3 = document.querySelector("#button3");
+
+btn3.addEventListener("click", function () {
+    btn3.classList.toggle("darkmode");
+})
+
+// event
+document.querySelector("body").addEventListener("mousemove", function (e) {
+    console.log(e.clientX, e.clientY);
+})
